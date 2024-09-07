828dded354dba5e72efc988214d24ae146f5de42
Milind <m.milind15@gmail.com>
2024-09-07 09:35:43 +0530

    pls fix cors

--- client/next.config.mjs
+++ client/next.config.mjs
@@ -4,16 +4,27 @@ const nextConfig = {
   async headers() {
     return [
       {
-        // matching all API routes
-        source: "*",
+        source: '/(.*)',
         headers: [
-          { key: "Access-Control-Allow-Credentials", value: "true" },
-          { key: "Access-Control-Allow-Origin", value: "*" }, // replace this your actual origin
-          { key: "Access-Control-Allow-Methods", value: "GET,DELETE,PATCH,POST,PUT" },
-          { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
-        ]
-      }
-    ]
+          {
+            key: 'Access-Control-Allow-Origin',
+            value: 'https://dreamteam.milind.lol',
+          },
+          {
+            key: 'Access-Control-Allow-Methods',
+            value: 'GET,OPTIONS,POST,PUT,DELETE',
+          },
+          {
+            key: 'Access-Control-Allow-Headers',
+            value: 'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept',
+          },
+          {
+            key: 'Access-Control-Allow-Credentials',
+            value: 'true',
+          },
+        ],
+      },
+    ];
   },
 
   images: {
