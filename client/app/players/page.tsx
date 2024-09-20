a6562caa8fbc90a1cca8a68aa72ec7368c42754c
Milind <m.milind15@gmail.com>
2024-09-20 13:34:25 +0530

    Make all players server rendered

--- client/app/players/page.tsx
+++ client/app/players/page.tsx
@@ -1,42 +1,24 @@
-"use client";
-
 import { Player } from "@/types/player";
-import { useEffect, useState } from "react";
 import PlayerCard from "@/components/PlayerCard";
-import Spinner from "@/components/Spinner/Spinner";
-export default function PlayersPage() {
 
-  const [players, setPlayers] = useState<Player[]>([]);
-  const [isLoading, setIsLoading] = useState(true);
-  useEffect(() => {
-    setIsLoading(true);
+async function getPlayers(): Promise<Player[]> {
+  const res = await fetch(`${process.env.NEXT_PUBLIC_BACKEND_URL}/players/all`, { cache: 'no-store' });
+  if (!res.ok) {
+    throw new Error('Failed to fetch players');
+  }
+  return res.json();
+}
 
-    fetch(`${process.env.NEXT_PUBLIC_BACKEND_URL}/players/all`)
-      .then((res) => res.json())
-      .then((data) => {
-        setPlayers(data);
-        setIsLoading(false);
-      })
-      .catch((error) => {
-        console.error("Error fetching players:", error);
-        setIsLoading(false);
-      });
-  }, []);
-
+export default async function PlayersPage() {
+  const players = await getPlayers();
 
   return (
     <div className="container mx-auto px-4 py-8">
-      {isLoading ? (
-        <div className="flex justify-center items-center h-screen">
-          <Spinner />
-        </div>
-      ) : (
-        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 xl:grid-cols-4 gap-8">
-          {players?.map((player) => (
-            <PlayerCard key={player.id} {...player} />
-          ))}
-        </div>
-      )}
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 xl:grid-cols-4 gap-8">
+        {players.map((player) => (
+          <PlayerCard key={player.id} {...player} />
+        ))}
+      </div>
     </div>
   );
 }
