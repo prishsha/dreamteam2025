e460a73ddf54ec1b52a735f047bec764879c85af
Milind <m.milind15@gmail.com>
2024-09-20 19:04:07 +0530

    auction page improved

--- client/app/auction/page.tsx
+++ client/app/auction/page.tsx
@@ -8,6 +8,7 @@ import { showToast, ToastType } from "@/utils/toast";
 import Image from "next/image";
 import { useEffect, useState } from "react";
 import RatingHolder from '@/assets/RatingHolder.svg';
+import GetCountryFlagIcon from "@/utils/flags";
 
 export default function AuctionPage() {
   const [socket, setSocket] = useState<WebSocket | null>(null);
@@ -55,7 +56,7 @@ export default function AuctionPage() {
               showToast(`${assignPlayerMessage.player.name} has been assigned to ${assignPlayerMessage.participatingTeam.name} for ${humanizePrice(assignPlayerMessage.bidAmount)}!`, ToastType.SUCCESS);
             }
 
-            if(assignPlayerMessage.type === "unsoldPlayer") {
+            if (assignPlayerMessage.type === "unsoldPlayer") {
               showToast(`${unsoldPlayerMessage.player.name} went unsold!`, ToastType.WARNING);
             }
           }
@@ -94,63 +95,60 @@ export default function AuctionPage() {
       }
     };
   }, []);
-
   return (
     <div className="min-h-screen text-white p-8 flex flex-col items-center justify-center" style={getGradient()}>
       {gameState?.IsBiddingActive && gameState.CurrentPlayerInBid ? (
-        <>
-
-          <div className="absolute top-8 left-8 z-10">
-            <div className="relative w-36 h-36">
-              <Image src={RatingHolder} alt="Star" layout="fill" className="text-yellow-300" />
-              <span className="absolute inset-0 flex items-center justify-center text-6xl font-bold text-white">{gameState.CurrentPlayerInBid.rating}</span>
+        <div className="flex flex-col md:flex-row items-center justify-between w-full h-full gap-8">
+          <div className="flex-1 flex flex-col items-center justify-center h-full z-10 relative">
+            <div className="absolute inset-0 z-0 m-6">
+              <Image
+                src={`${process.env.NEXT_PUBLIC_BACKEND_URL}/assets/cardbg.png`}
+                alt="Dream Team Logo"
+                className='opacity-30'
+                layout='fill'
+                objectFit='cover'
+              />
             </div>
-          </div>
-
-          <div className="absolute top-8 right-8 z-10">
-            <div className="relative w-48 h-48">
-              <Image src={`${process.env.NEXT_PUBLIC_BACKEND_URL}/assets/dreamteamsmall.png`} alt={gameState.CurrentPlayerInBid.country} layout="fill" objectFit="cover" />
-            </div>
-          </div>
-
-          <div className="flex flex-col md:flex-row items-center justify-between w-full h-full">
-            <div className="flex-1 flex flex-col items-center justify-center h-full z-10 relative">
-              <div className="absolute inset-0 z-0 m-6">
-                <Image
-                  src={`${process.env.NEXT_PUBLIC_BACKEND_URL}/assets/cardbg.png`}
-                  alt="Dream Team Logo"
-                  className='opacity-30'
-                  layout='fill'
-                  objectFit='cover'
-                />
-              </div>
-              {gameState.CurrentPlayerInBid.avatarUrl.Valid && (
-                <Image
-                  width={512}
-                  height={512}
-                  src={`${gameState.CurrentPlayerInBid.avatarUrl.String}`}
-                  alt={gameState.CurrentPlayerInBid.name}
-                  className="w-full h-4/5 md:h-3/5 object-contain mb-4 relative z-10"
-                  style={{ maxHeight: '80vh' }}
-                />
-              )}
-              <div className="w-full border-t-2 border-b-2 border-white py-4 text-center relative z-10">
-                <h2 className="text-6xl md:text-5xl font-bold">{gameState.CurrentPlayerInBid.name}</h2>
+            <div className="absolute top-12 left-8 z-10">
+              <div className="relative w-24 h-24 md:w-24 md:h-24 sm:w-12 sm:h-12">
+                <Image src={RatingHolder} alt="Star" layout="fill" className="text-yellow-300" />
+                <span className="absolute inset-0 flex items-center justify-center text-4xl font-bold text-white">{gameState.CurrentPlayerInBid.rating}</span>
               </div>
             </div>
-            <div className="text-left flex-1 mt-8 md:mt-0 z-10">
-              <h1 className="text-5xl font-bold mb-8">Current Player</h1>
-              <p className="text-2xl mb-2">{gameState.CurrentPlayerInBid.country}</p>
-              <p className="text-2xl mb-2">Role: {gameState.CurrentPlayerInBid.role}</p>
-              <p className="text-2xl mb-8">Rating: {gameState.CurrentPlayerInBid.rating}</p>
 
-              <div>
-                <p className="text-6xl font-bold mb-4">Current Bid: <span className="text-green-400">₹{humanizePrice(gameState.CurrentBidAmount)}</span></p>
-                <p className="text-4xl">Next Bid: <span className="text-yellow-400">₹{humanizePrice(gameState.NextBidAmount)}</span></p>
+            <div className="absolute top-12 right-8 z-10">
+              <div className="relative w-24 h-24 md:w-24 md:h-24 sm:w-12 sm:h-12">
+                <Image src={GetCountryFlagIcon(gameState.CurrentPlayerInBid.country)} alt={gameState.CurrentPlayerInBid.country} layout="fill" objectFit="cover" />
               </div>
             </div>
+            {gameState.CurrentPlayerInBid.avatarUrl.Valid && (
+              <Image
+                width={512}
+                height={512}
+                src={`${gameState.CurrentPlayerInBid.avatarUrl.String}`}
+                alt={gameState.CurrentPlayerInBid.name}
+                className="w-full h-4/5 md:h-3/5 object-contain relative z-10"
+                style={{ maxHeight: '80vh' }}
+              />
+            )}
+            <div className="w-full py-4 text-center relative z-10" style={{ backgroundColor: teamColour?.end }}>
+              <h2 className="text-6xl md:text-5xl font-bold">{gameState.CurrentPlayerInBid.name}</h2>
+            </div>
           </div>
-        </>
+          <div className="text-left flex-1 mt-8 md:mt-0 z-10 md:ml-8">
+            <div className="absolute top-8 right-8 z-10">
+              <div className="relative w-48 h-48">
+                <Image src={`${process.env.NEXT_PUBLIC_BACKEND_URL}/assets/dreamteamsmall.png`} alt={gameState.CurrentPlayerInBid.country} layout="fill" objectFit="cover" className="hidden md:block" />
+              </div>
+            </div>
+            <h1 className="text-7xl font-bold mb-8 text-yellow-400">{gameState.CurrentPlayerInBid.country}</h1>
+            <p className="text-5xl mb-8 text-green-400">{gameState.CurrentPlayerInBid.role}</p>
+            <div className="bg-opacity-50 bg-black p-6 rounded-lg">
+              <p className="text-7xl font-bold mb-4">Current Bid: <span className="text-green-400">₹{humanizePrice(gameState.CurrentBidAmount)}</span></p>
+              <p className="text-4xl">Next Bid: <span className="text-yellow-400">₹{humanizePrice(gameState.NextBidAmount)}</span></p>
+            </div>
+          </div>
+        </div>
       ) : (
         <p className="text-4xl font-bold">Waiting for the auction to start...</p>
       )}
